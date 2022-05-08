40a4248d6e6d28d67da2478d6eba9201ff9896ec
ayumiohno <ayumi0130ohno@gmail.com>
2022-05-08 09:06:01 +0900

    bad_allocが出る

--- 01_anaglam/problem1/main.cpp
+++ 01_anaglam/problem1/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
-	
+
+	std::ifstream ifs("./words.txt");
+	int buf_size = 81;
+	char str[buf_size];
+	int line_num = 0;
+	std::vector<std::string> dictionary;
+	std::string word;
+	if(ifs.fail()){
+		std::cout << "failed to read file" << std::endl;
+		return -1;
+	}
+	while (ifs.getline(str, buf_size)){
+		word = str;
+		std::sort(word.begin(), word.end());
+		do{	
+			dictionary.push_back(word);
+		}while(std::next_permutation(word.begin(), word.end()));
+		++ line_num;
+	}
+	std::cout << line_num << " " << dictionary.size() << std::endl;
+	for (auto word : dictionary){
+		std::cout << word << std::endl;
+	}
+
+
+	return 0;
 }
