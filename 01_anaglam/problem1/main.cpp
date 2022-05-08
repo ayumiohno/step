cde011b0f29f68273cbe52dfeadd46e0e927413c
ayumiohno <ayumi0130ohno@gmail.com>
2022-05-08 19:56:04 +0900

    problem1 n!が大きすぎて計算が終わらない

--- 01_anaglam/problem1/main.cpp
+++ 01_anaglam/problem1/main.cpp
@@ -2,33 +2,54 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include "permutation.hpp"
 
 int main()
 {
 
-	std::ifstream ifs("./words.txt");
+	std::ifstream dic("./test.txt");
 	int buf_size = 81;
 	char str[buf_size];
 	int line_num = 0;
 	std::vector<std::string> dictionary;
 	std::string word;
-	if(ifs.fail()){
+	if(dic.fail()){
 		std::cout << "failed to read file" << std::endl;
 		return -1;
 	}
-	while (ifs.getline(str, buf_size)){
+	while (dic.getline(str, buf_size)){
 		word = str;
-		std::sort(word.begin(), word.end());
+		std::cout << word << std::endl;
+		auto permutation = Permutation{word};
+		do{
+			dictionary.push_back(permutation.getList());
+		}while(permutation.permutate());
+
+/*		std::sort(word.begin(), word.end(), std::greater<>());
 		do{	
 			dictionary.push_back(word);
 		}while(std::next_permutation(word.begin(), word.end()));
-		++ line_num;
+		++ line_num;*/
+
 	}
-	std::cout << line_num << " " << dictionary.size() << std::endl;
+
 	for (auto word : dictionary){
 		std::cout << word << std::endl;
 	}
 
+	std::sort(dictionary.begin(), dictionary.end());
+
+	std::ifstream words("./testcase/test.txt");
+	if(dic.fail()){
+		std::cout << "failed to read file" << std::endl;
+		return -1;
+	}
+	while (dic.getline(str, buf_size)){
+		word = str;
+		if(*(std::lower_bound(dictionary.begin(), dictionary.end(), word)) != word ){
+			std::cout << word << std::endl;
+		}
+	}
 
 	return 0;
 }
