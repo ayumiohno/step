2e1fa7fc8dcb89643e51c4214302d134b21964bf
ayumiohno <ayumi0130ohno@gmail.com>
2022-05-28 09:34:11 +0900

    ファイル読み込み module化

---
+++ 04_graph/main.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+
+void readFile(const std::string&& file_name,
+    const std::function<void(std::string, std::string)>&& process_data)
+{
+    std::ifstream file(file_name);
+    std::string data;
+    while (std::getline(file, data)) {
+        auto index = data.find('\t');
+        auto arg1 = data.substr(0, index);
+        auto arg2 = data.substr(index + 1, data.size() - arg1.size() - 1);
+        process_data(std::move(arg1), std::move(arg2));
+    }
+    file.close();
+}
+
+int main()
+{
+    std::map<std::string, std::string> pages;
+    std::map<std::string, std::set<std::string>> links;
+    readFile("testcase/pages_small.txt", [&pages](auto arg1, auto arg2) { pages[arg1] = arg2; });
+    readFile("testcase/links_small.txt", [&links](auto arg1, auto arg2) { links[arg1].insert(arg2); });
+
+
+    for (const auto& page : pages) {
+        if (page.second == "Google") {
+            std::cout << page.second << " " << page.first << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
