3876b98de3a1ede1f3686289c3a2e7fc027c18c9
ayumiohno <ayumi0130ohno@gmail.com>
2022-06-07 05:49:31 +0900

    05_tsp

--- 05_tsp/randomized_algorithm/tsp.cpp
+++ 05_tsp/randomized_algorithm/tsp.cpp
@@ -4,15 +4,16 @@
 #include <iostream>
 #include <utility>
 
-void swapPoints(const int& swap1, const int& swap2, 
-        std::vector<Point>& points, double& min_length, const int& num_of_dots)
+/* 1 <= swap1, swap2  <= points.size() - 1 */
+void swapPoints(const int& swap1, const int& swap2,
+    std::vector<Point>& points, double& min_length, const int& num_of_dots)
 {
-    double length_diff = 0; //入れ替えた後の長さ合計 - 入れ替えた後の長さ合計
-    length_diff -= (points.at(swap1) - points.at((swap1 - 1 + num_of_dots) % num_of_dots)).getNolm();
-    length_diff += (points.at(swap2) - points.at((swap1 - 1 + num_of_dots) % num_of_dots)).getNolm();
+    double length_diff = 0;  //入れ替えた後の長さ合計 - 入れ替えた後の長さ合計
+    length_diff -= (points.at(swap1) - points.at(swap1 - 1)).getNolm();
+    length_diff += (points.at(swap2) - points.at(swap1 - 1)).getNolm();
     length_diff -= (points.at(swap2) - points.at((swap2 + 1) % num_of_dots)).getNolm();
     length_diff += (points.at(swap1) - points.at((swap2 + 1) % num_of_dots)).getNolm();
-    if (length_diff < 0) { //入れ替えると長さが短くなる場合
+    if (length_diff < 0) {  //入れ替えると長さが短くなる場合
         min_length += length_diff;
         std::reverse(points.begin() + swap1, points.begin() + swap2 + 1);
     }
