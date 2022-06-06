a5b62dcb7b336b0dc379b64a2fcc4cc4702d6dd1
ayumiohno <ayumi0130ohno@gmail.com>
2022-06-07 00:31:46 +0900

    05_tsp

--- 05_tsp/determistic_algorithm/main.sh
+++ 05_tsp/determistic_algorithm/main.sh
@@ -3,12 +3,10 @@ read -p "Press file number : " file_num
 if [ -e output_$file_num.csv ]; then
 	rm output_$file_num.csv
 fi
-if [ ! -e a.out ] || [ a.out -ot main.cpp ]; then
-	g++ -O2 main.cpp tsp.cpp -o a.out
-	if [ $? != 0 ]; then
-		echo "failed to compile." >&2
-		exit 1
-	fi
+g++ -O2 main.cpp tsp.cpp -o a.out
+if [ $? != 0 ]; then
+	echo "failed to compile." >&2
+	exit 1
 fi
 ./a.out <<EOS > output_$file_num.csv
 $file_num
