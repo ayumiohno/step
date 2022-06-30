a09bffd4dc8dadd7199884df3441c5e80a01faaa
ayumiohno <ayumi0130ohno@gmail.com>
2022-06-30 16:51:10 +0900

    分散処理　テスト

--- 05_tsp/communication/server_for_ga.cpp
+++ 05_tsp/communication/server_for_ga.cpp
@@ -12,7 +12,7 @@
 #include <unistd.h>
 #include <vector>
 
-constexpr int FILE_NUM = 5;
+constexpr int FILE_NUM = 4;
 constexpr std::array<int, 8> NUMS = {5, 8, 16, 64, 128, 512, 2048, 8192};
 constexpr int NUM_OF_CITY = NUMS.at(FILE_NUM);
 
